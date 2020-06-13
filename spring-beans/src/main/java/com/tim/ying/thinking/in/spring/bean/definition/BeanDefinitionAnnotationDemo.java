97e5d82f4dc39a681eb59654b1c9233395620102
应碧波 <yingbibo@canzhaoxi.com.cn>
2020-06-13 23:24:12 +0800

    feat(BeanAnnotation):BeanDefinitionBuilder 注册bean方式

--- spring-beans/src/main/java/com/tim/ying/thinking/in/spring/bean/definition/BeanDefinitionAnnotationDemo.java
+++ spring-beans/src/main/java/com/tim/ying/thinking/in/spring/bean/definition/BeanDefinitionAnnotationDemo.java
@@ -1,13 +1,17 @@
 package com.tim.ying.thinking.in.spring.bean.definition;
 
 import com.tim.ying.thinking.in.spring.bean.pojo.User;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionReaderUtils;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 /**
- * AnnotationContext 注解定义Bean的方式三种
+ * AnnotationContext 注解定义Bean的方式三种常规方式
  * 1. @Component 及派生类
  * 2. @Bean 注解
  * 3. @Import 注解
@@ -24,11 +28,29 @@ public class BeanDefinitionAnnotationDemo {
 		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
 		applicationContext.register(BeanDefinitionAnnotationDemo.class);
 		applicationContext.refresh();
+
+		registerByBeanDefinitionBuilder(applicationContext,"anotherUser");
+		registerByBeanDefinitionBuilder(applicationContext,null);
+
 		System.out.println("BeanDefinitionAnnotationDemo bean - > " + applicationContext.getBeansOfType(BeanDefinitionAnnotationDemo.class));
 		System.out.println("Config bean - > " + applicationContext.getBeansOfType(Config.class));
 		System.out.println("User bean - > " + applicationContext.getBeansOfType(User.class));
 		System.out.println("ImportConfig bean - > " + applicationContext.getBeansOfType(ImportConfig.class));
 		applicationContext.close();
+	}
+
+
+
+	public static void registerByBeanDefinitionBuilder(BeanDefinitionRegistry registry,String beanName){
+		BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(User.class)
+				.addPropertyValue("id",2L).addPropertyValue("name","JAVA-TIM");
+
+		if(StringUtils.hasText(beanName)){
+			registry.registerBeanDefinition(beanName,builder.getBeanDefinition());
+		}else{
+			BeanDefinitionReaderUtils.registerWithGeneratedName(builder.getBeanDefinition(),registry);
+		}
+
 	}
 
 	@Component
